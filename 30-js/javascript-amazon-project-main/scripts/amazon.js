995839ba6a561337a85c40ee1473b9227d5fbcf3
DoNM <manhdo.it@gmail.com>
2024-08-17 13:24:08 +0700

    make the cart quantity interactive

--- 30-js/javascript-amazon-project-main/scripts/amazon.js
+++ 30-js/javascript-amazon-project-main/scripts/amazon.js
@@ -65,7 +65,6 @@ productGrid.innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 	button.addEventListener('click', () => {
 		const productId = button.dataset.productId;
-
 		let matchingItem;
 		cart.forEach((item) => {
 			if (productId === item.productId) {
@@ -81,7 +80,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
 				quantity: 1,
 			});
 		}
-		
-		console.log(cart);
+		let cartQuantity = 0;
+		cart.forEach((item) => {
+			cartQuantity += item.quantity;
+		});
+		document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+		console.log(cart, cartQuantity);
 	})
 })
+
