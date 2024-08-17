bce36579f04b465dfd4f1fac7919be4150d0da4e
DoNM <manhdo.it@gmail.com>
2024-08-17 13:17:24 +0700

    Make add to cart button interactive

--- 30-js/javascript-amazon-project-main/scripts/amazon.js
+++ 30-js/javascript-amazon-project-main/scripts/amazon.js
@@ -1,42 +1,3 @@
-const products = [
-	{
-		image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-		name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-		rating: {
-			stars: 4.5,
-			count: 87
-		},
-		priceCents: 1090
-	},
-	{
-		image: 'images/products/intermediate-composite-basketball.jpg',
-		name: 'Intermediate Size Basketball',
-		rating: {
-			stars: 4,
-			count: 127
-		},
-		priceCents: 2095
-	},
-	{
-		image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-		name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-		rating: {
-			stars: 4.5,
-			count: 56
-		},
-		priceCents: 799
-	},
-	{
-		image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-		name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-		rating: {
-			stars: 4.5,
-			count: 87
-		},
-		priceCents: 1090
-	}
-];
-
 let productsHTML = ''
 
 products.forEach((product) => {
@@ -86,7 +47,10 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+					data-product-name="${product.name}"
+					data-product-id="${product.id}"
+					>
             Add to Cart
           </button>
         </div>
@@ -95,3 +59,29 @@ products.forEach((product) => {
 
 const productGrid = document.querySelector('.js-product-grid');
 productGrid.innerHTML = productsHTML;
+
+
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+	button.addEventListener('click', () => {
+		const productId = button.dataset.productId;
+
+		let matchingItem;
+		cart.forEach((item) => {
+			if (productId === item.productId) {
+					matchingItem = item;
+				}
+		});
+
+		if(matchingItem) {
+			matchingItem.quantity += 1;
+		} else {
+			cart.push({
+				productId: productId,
+				quantity: 1,
+			});
+		}
+		
+		console.log(cart);
+	})
+})
