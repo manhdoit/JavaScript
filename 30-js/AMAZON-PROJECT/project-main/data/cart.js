06af423d1abe362a4786a7a87e62a2617b3bd1e9
DoNM <manhdo.it@gmail.com>
2024-08-24 09:17:01 +0700

    3

--- 30-js/AMAZON-PROJECT/project-main/data/cart.js
+++ 30-js/AMAZON-PROJECT/project-main/data/cart.js
@@ -1,13 +1,23 @@
-export const cart = [
-	{
-		productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-		quantity: 2
-	},
-	{
-		productId: 'dd82ca78-a18b-4e2a-9250-31e67412f98d',
-		quantity: 12
-	}
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart) {
+	cart = [
+		{
+			productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+			quantity: 2
+		},
+		{
+			productId: 'dd82ca78-a18b-4e2a-9250-31e67412f98d',
+			quantity: 12
+		}
+	];
+}
+
+
+function saveToStorage() {
+	localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 
 export function addToCard(productId) {
 	let matchingItem;
@@ -25,6 +35,8 @@ export function addToCard(productId) {
 			quantity: 1,
 		});
 	}
+
+	saveToStorage();
 }
 
 export  function updateCartQuantity() {
@@ -34,4 +46,18 @@ export  function updateCartQuantity() {
 	});
 	document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 	console.log(cart, cartQuantity);
+}
+
+
+export function removeFromCart(productId) {
+	const newCart = [];
+
+	cart.forEach((cartItem) => {
+		if(cartItem.productId !== productId) {
+			newCart.push(cartItem);
+		}
+	});
+	cart = newCart;
+	
+	saveToStorage();
 }
--- 30-js/AMAZON-PROJECT/project-main/scripts/checkout.js
+++ 30-js/AMAZON-PROJECT/project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import { cart, removeFromCart } from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 let cartSummaryHTML = '';
@@ -11,10 +11,10 @@ cart.forEach((cartItem) => {
 			matchingProduct = product
 		}
 	})
-	console.log(matchingProduct);
+
 	cartSummaryHTML += 
 	`
-	<div class="cart-item-container">
+	<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
 		<div class="delivery-date">
 			Delivery date: Tuesday, June 21
 		</div>
@@ -39,7 +39,7 @@ cart.forEach((cartItem) => {
 					<span class="update-quantity-link link-primary">
 						Update
 					</span>
-					<span class="delete-quantity-link link-primary">
+					<span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchingProduct.id}>
 						Delete
 					</span>
 				</div>
@@ -94,5 +94,24 @@ cart.forEach((cartItem) => {
 	`;
 });
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+if(cart.length >= 1){
+	document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+	
+} else {
+	document.querySelector('.js-order-summary').innerHTML = 'Your cart is Empty';
+	
+}
+console.log(!cart);
 
+document.querySelectorAll('.js-delete-link')
+	.forEach((link) => {
+		link.addEventListener('click', () => {
+			const productId = link.dataset.productId;
+			removeFromCart(productId);
+			const container = document.querySelector(
+				`.js-cart-item-container-${productId}`
+			);
+			container.remove();
+			console.log(cart);
+		})
+	})
