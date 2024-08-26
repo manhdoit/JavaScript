1793d76ef673394175f5f44d8c11a2647777464a
DoNM <manhdo.it@gmail.com>
2024-08-27 01:13:01 +0700

    1

--- 30-js/AMAZON-PROJECT/project-main/scripts/checkout.js
+++ 30-js/AMAZON-PROJECT/project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, totalItemCount, addToCard } from '../data/cart.js';
+import { cart, removeFromCart, totalItemCount, addToCard, updateDeliveryOption } from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -96,7 +96,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 		const isChecked = deliveryOption.id === cartItem.deliveryOptionId
 		html += 
 		`
-				<div class="delivery-option">
+				<div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
 					<input type="radio" ${isChecked ? 'checked' : ''}
 						class="delivery-option-input"
 						name="delivery-option-${matchingProduct.id}">
@@ -166,3 +166,11 @@ saveQuantityLinks.forEach((link) => {
 	})
 })
 
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+	element.addEventListener('click', () => {
+		const {productId, deliveryOptionId} = element.dataset;
+		updateDeliveryOption(productId, deliveryOptionId);
+		console.log(cart)
+	})
+})
