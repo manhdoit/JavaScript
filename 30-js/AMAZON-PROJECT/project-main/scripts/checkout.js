afd119717adc49f1a39cc017f5927701a334c1c5
DoNM <manhdo.it@gmail.com>
2024-08-27 01:55:11 +0700

    save data and generate HTML for Payment Summary

--- 30-js/AMAZON-PROJECT/project-main/scripts/checkout.js
+++ 30-js/AMAZON-PROJECT/project-main/scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 renderOderSummary();
+renderPaymentSummary();
